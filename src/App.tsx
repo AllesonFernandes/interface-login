dab3a24e2740cf752e4c9cf03648b7e6c74c5a70
Alleson Fernandes <75983438+AllesonFernandes@users.noreply.github.com>
2025-04-09 19:19:21 +0000

    Feat: finalização

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,56 @@
-import { Card, CardHeader, CardTitle, CardDescription, CardContent} from "./components/ui/card"
-import { Label } from "./components/ui/label"
-import { Input } from "./components/ui/input"
-import { Button } from "./components/ui/button"
-import { Separator } from "./components/ui/separator"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent} from "./components/ui/card";
+import { Label } from "./components/ui/label";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import student1 from "../src/assets/student1.svg";
+import student2 from "../src/assets/student2.svg";
+import { Carousel, CarouselItem, CarouselContent, CarouselPrevious, CarouselNext } from "../src/components/ui/carousel";
+
+
 export function App() {
 
 
   return (
     <>
       <main className="h-screen flex w-full">
-        <div className="flex bg-primary-foreground w-full h-full p-16"></div>
+        <div className="flex items-center justify-center bg-stone-100 w-full h-full p-16">
+          <Carousel className="w-full max-w-xl">
+            <CarouselContent>
+              <CarouselItem>
+                <div className="flex">
+                  <img src={student1} alt="estudante estressada" />
+                </div>
+              </CarouselItem>
+              <CarouselItem>
+                <div className="flex">
+                  <img src={student2} alt="outro estudante estressado" />
+                </div>
+              </CarouselItem>
+            </CarouselContent>
+            
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
         <section className="flex items-center justify-center bg-background h-full max-w-3xl w-full p-4">
-          <Card>
+          <Card className="w-md">
             <CardHeader>
               <CardTitle className="text-2xl font-bold tracking-tighter">
-                Faça Login
+                Crie uma nova conta
               </CardTitle>
               <CardDescription>
                 Utilize um email e crie uma nova senha
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <div>
+              <div className="gap-2">
                 <Label htmlFor="email">E-mail</Label>
-                <Input id="email" placeholder="exemplo@email.com" type="email"></Input>
+                <Input id="email" placeholder="exemplo@email.com" type="email" className="mt-2"></Input>
               </div>
               <div className="mt-4">
                 <Label htmlFor="password">Senha</Label>
-                <Input id="FirstPassword" placeholder="Crie uma senha" type="password"></Input>
+                <Input id="FirstPassword" placeholder="Crie uma senha" type="password" className="mt-2"></Input>
               </div>
               <div className="mt-4">
                 <Input id="SecondPassword" placeholder="Repite a senha" type="password"></Input>
@@ -38,7 +61,7 @@ export function App() {
                 <span className="text-xs text-muted-foreground">OU</span>
                 <Separator />
               </div>
-              <Button variant={"outline"} className="mt-4 w-full">Entre com sua conta</Button>
+              <Button variant={"outline"} className="mt-4 w-full">Entre com sua conta Google</Button>
             </CardContent>
           </Card>
         </section>
